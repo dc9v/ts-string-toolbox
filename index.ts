8b90ea6583416a0154ee10a40384c2e8a1372361
Taegyun, Ko <taegyun.ko@metervision.io>
2022-07-17 22:52:38 +0900

    fixed some error in index

--- index.ts
+++ index.ts
@@ -1,9 +1,2 @@
-import * as pluralize from 'pluralize';
 import CuteStringUtil from './cute';
-
-export function getPlural(str: any): string
-{
-   return pluralize.plural(str);
-}
-
 export { CuteStringUtil as default };
